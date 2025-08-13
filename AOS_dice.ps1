76f3dc80932987d622d5dc47c548e7e116713e8f
Theo Bird <52682333+Elemental-IT@users.noreply.github.com>
2025-08-13 12:53:11 +1000

    Create AOS_dice.ps1
    posting

---
+++ AOS_dice.ps1
@@ -0,0 +1,58 @@
+# Author Theo bird
+
+# Change the variables to modify the dice outputs 
+# Crit Rolls are Mortal wounds for the time being I will add order wounds when I give this UI
+# variables
+$Attacks      = 61
+$Crit         = 5
+$Hit          = 2
+$Wound        = 3
+$Save         = 4
+$Rend         = 0
+$Damage       = 1
+$Attackrolls  = 1..$Attacks
+$CritRolls    = $null
+$HitRolls     = $null
+$WoundRolls   = $null
+$SaveRolls    = $null 
+$DamageTotal  = $null
+$HitRollsArr  = @()
+$WoundRollArr   = @()
+
+function D6 {Get-Random -Minimum 1 -Maximum 7}
+function D3 {Get-Random -Minimum 1 -Maximum 3}
+ 
+$Attackrolls   | ForEach-Object { $HitRoll = D6 ; $HitRollsArr += $HitRoll ; if($HitRoll -ge $Crit){$DamageTotal += $Damage;$CritRolls++}Else{if(($HitRoll -ge $Hit) -and ($HitRoll -lt $Crit)){$HitRolls++}}}
+1..$HitRolls   | ForEach-Object { $WoundRoll = D6 ; $WoundRollArr += $WoundRoll ; if($WoundRoll -ge $Wound){$WoundRolls++}}
+1..$WoundRolls | ForEach-Object { $SaveRoll = D6 ; if($SaveRoll -ge ($save - $Rend)){$SaveRolls++}}
+$DamageTotal += $SaveRolls * $Damage 
+
+
+Write-Host "Crit hits $CritRolls"
+Write-Host "hits $HitRolls"
+Write-Host "Wounds $WoundRolls"
+Write-Host "fail Saves $SaveRolls"
+Write-Host "Total Damage $DamageTotal"
+Write-Host $null
+
+Write-Host "Hit rolls"
+$HitRollsArr = $HitRollsArr | Group-Object | Sort-Object Name | ForEach-Object {
+    [PSCustomObject]@{
+        Name  = $_.Name
+        Count = $_.Count
+    }
+}
+
+$HitRollsArr | ft
+
+
+Write-Host "Wound rolls"
+$WoundRollArr = $WoundRollArr | Group-Object | Sort-Object Name | ForEach-Object {
+    [PSCustomObject]@{
+        Name  = $_.Name
+        Count = $_.Count
+    }
+}
+
+
+$WoundRollArr | ft
